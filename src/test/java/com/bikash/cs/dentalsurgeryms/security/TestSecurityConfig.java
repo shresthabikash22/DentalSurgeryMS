8e10f871ac9a4e0dd5f5a4b65dd1990271d95d54
shresthabikash22 <shresthabikash22@gmail.com>
2025-04-26 17:11:05 -0500

    disabled spring security in JUnit Tests

---
+++ src/test/java/com/bikash/cs/dentalsurgeryms/security/TestSecurityConfig.java
@@ -0,0 +1,16 @@
+package com.bikash.cs.dentalsurgeryms.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@TestConfiguration
+public class TestSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+
+}

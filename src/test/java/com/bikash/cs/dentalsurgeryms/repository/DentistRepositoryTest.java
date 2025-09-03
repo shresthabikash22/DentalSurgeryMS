9eeee4c5eac84c3b7937b4e73b0ae482390ac2b8
shresthabikash22 <shresthabikash22@gmail.com>
2025-09-03 16:53:17 -0500

    Update DentistRepositoryTest.java

--- src/test/java/com/bikash/cs/dentalsurgeryms/repository/DentistRepositoryTest.java
+++ src/test/java/com/bikash/cs/dentalsurgeryms/repository/DentistRepositoryTest.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@ActiveProfiles("test") 
 @DataJpaTest
 @ActiveProfiles("test")
 class DentistRepositoryTest {

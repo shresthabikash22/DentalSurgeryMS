a4b9d097e1cc128447afd5a3a59f02ee987ac1c0
shresthabikash22 <shresthabikash22@gmail.com>
2025-09-03 16:56:55 -0500

    Update AppointmentControllerTest.java

--- src/test/java/com/bikash/cs/dentalsurgeryms/controller/AppointmentControllerTest.java
+++ src/test/java/com/bikash/cs/dentalsurgeryms/controller/AppointmentControllerTest.java
@@ -21,6 +21,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
 
